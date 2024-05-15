6d5ce7f1e2a4cf050460e1cd7a1a7a5409938f87
le tri <triledacminh@gmail.com>
2024-05-15 10:40:53 +0700

    6 pattern

--- TicTacToeAi.py
+++ TicTacToeAi.py
@@ -1,9 +1,86 @@
 import copy
 import random
 import time
-global max_depth, hashT, HEURISTIC_SOCRES, R, Q, R4, R5, R6, count, count_leaf
+global max_depth, hashT, HEURISTIC_SOCRES, R, Q, R4, R5, R6, count, count_leaf, delay_time
 max_depth = 6
+delay_time = 0
 
+HEURISTIC_SCORES = {
+'ooooox' : -1000000000001,
+'ooooo ' : -1000000000001,
+'xooooo' : -1000000000001,
+' ooooo' : -1000000000001,
+'xxxxxo' : 1000000000001,
+'xxxxx ' : 1000000000001,
+'oxxxxx' : 1000000000001,
+' xxxxx' : 1000000000001,
+' oooo ' : -10000000002,
+' xxxx ' : 10000000002,
+'oxxxx ' : 100000000,
+' xxxxo' : 100000000,
+' oooox' : -100000000,
+'xoooo ' : -100000000,
+' xxx x' : 1000,
+' xxx o' : 1000,
+' xxx  ' : 1000,
+'x xxx ' : 1000,
+'o xxx ' : 1000,
+'  xxx ' : 1000,
+' ooo x' : -1000,
+' ooo o' : -1000,
+' ooo  ' : -1000,
+'x ooo ' : -1000,
+'o ooo ' : -1000,
+'  ooo ' : -1000,
+' xxxox' : 150,
+' xxxoo' : 150,
+' xxxo ' : 150,
+'x xxxo' : 150,
+'  xxxo' : 150,
+' oooxx' : -150,
+' oooxo' : -150,
+' ooox ' : -150,
+'o ooox' : -150,
+'  ooox' : -150,
+'oxxx x' : 150,
+'oxxx  ' : 150,
+'xoxxx ' : 150,
+'ooxxx ' : 150,
+' oxxx ' : 150,
+'oxxx ' : 150,
+'xooo o' : -150,
+'xooo  ' : -150,
+'xxooo ' : -150,
+'oxooo ' : -150,
+' xooo ' : -150,
+'xooo ' : -150,
+' xx  x' : 10,
+' xx  o' : 10,
+' xx   ' : 10,
+'x xx  ' : 10,
+'o xx  ' : 10,
+' xx  ' : 10,
+'  xx x' : 10,
+'  xx o' : 10,
+'  xx  ' : 10,
+'x  xx ' : 10,
+'o  xx ' : 10,
+'   xx ' : 10,
+'  xx ' : 10,
+' oo  x' : -10,
+' oo  o' : -10,
+' oo   ' : -10,
+'x oo  ' : -10,
+'o oo  ' : -10,
+' oo  ' : -10,
+'  oo x' : -10,
+'  oo o' : -10,
+'  oo  ' : -10,
+'x  oo ' : -10,
+'o  oo ' : -10,
+'   oo ' : -10,
+'  oo ' : -10,
+}
 # HEURISTIC_SCORES = {
 #         "ooooo": -1000000000001,
 #         "xxxxx": 1000000000001,
@@ -26,45 +103,136 @@ max_depth = 6
 # }
 count = 0
 count_leaf = 0
+# hashT = {
+# #     6628 : -1000000000001,
+# # 22020 : 1000000000001,
+#     6628 : float("-inf"),
+#     22020 : float("inf"),
+#     19967 : -10000000002,
+#     7869 : 10000000002,
+#     8361 : 100000000,
+#     7948 : 100000000,
+#     20055 : -100000000,
+#     15645 : -100000000,
+#     2841 : 1000,
+#     965 : -1000,
+#     2920 : 150,
+#     1053 : -150,
+#     8425 : 150,
+#     20056 : -150,
+#     7794 : 10,
+#     24558 : 10,
+#     8222 : -10,
+#     9531 : -10,
+# }
 hashT = {
-    # 63350523 : -1000000000001,
-    # 85812864 : 1000000000001,
-    63350523 : float("-inf"),
-    85812864 : float("inf"),
-    129129796 : -10000000002,
-    151592128 : 10000000002,
-    213820052 : 100000000,
-    151592207 : 100000000,
-    129129884 : -100000000,
-    149759252 : -100000000,
-    30641524 : 1000,
-    199581976 : -1000,
-    30641603 : 150,
-    199582064 : -150,
-    214937516 : 150,
-    254753228 : -150,
-    30618996 : 10,
-    156884136 : 10,
-    199561752 : -10,
-    156292008 : -10,
+    20547 : float("-inf"),
+    20459 : float("-inf"),
+    15724 : float("-inf"),
+    20046 : float("-inf"),
+    3626 : float("inf"),
+    3547 : float("inf"),
+    8449 : float("inf"),
+    7957 : float("inf"),
+    19967 : -10000000002,
+    7869 : 10000000002,
+    8361 : 100000000,
+    7948 : 100000000,
+    20055 : -100000000,
+    15645 : -100000000,
+    12910 : 1000,
+    12901 : 1000,
+    12822 : 1000,
+    11937 : 1000,
+    16751 : 1000,
+    16259 : 1000,
+    27312 : -1000,
+    27303 : -1000,
+    27224 : -1000,
+    10061 : -1000,
+    14875 : -1000,
+    14383 : -1000,
+    5653 : 150,
+    5644 : 150,
+    5565 : 150,
+    12016 : 150,
+    16338 : 150,
+    22359 : -150,
+    22350 : -150,
+    22271 : -150,
+    14963 : -150,
+    14471 : -150,
+    13402 : 150,
+    13314 : 150,
+    17521 : 150,
+    22335 : 150,
+    21843 : 150,
+    8425 : 150,
+    22981 : -150,
+    22902 : -150,
+    1671 : -150,
+    6485 : -150,
+    5993 : -150,
+    20056 : -150,
+    16752 : 10,
+    16743 : 10,
+    16664 : 10,
+    16890 : 10,
+    21704 : 10,
+    7794 : 10,
+    21300 : 10,
+    21291 : 10,
+    21212 : 10,
+    6173 : 10,
+    10987 : 10,
+    10495 : 10,
+    24558 : 10,
+    16396 : -10,
+    16387 : -10,
+    16308 : -10,
+    17318 : -10,
+    22132 : -10,
+    8222 : -10,
+    21728 : -10,
+    21719 : -10,
+    21640 : -10,
+    18627 : -10,
+    23441 : -10,
+    22949 : -10,
+    9531 : -10,
 }
 R = 256
 # R6 = 205707292
 # R5 = 102220456
 # R4 = 128109484
-R6 = 102220456
-R5 = 128109484
+R6 = 5572
+# R5 = 16768
+
 # R4 = 16777216
-Q = 320527524
-# for key in HEURISTIC_SCORES:
+Q = 27481
+
+# def toHash(str):
 #     t  = 0
-#     for c in key:
+#     for c in str:
 #         t = (R * t + ord(c)) % Q
+#     return t
+#
+
+# for key in HEURISTIC_SCORES:
+#     t = toHash(key)
+#     # if len(key) == 5:
+#     #     print("\'" + key + "x" + "\'" + " : " + str(HEURISTIC_SCORES[key]) + ", ")
+#     #     print("\'" + key + "o" + "\'" + " : " + str(HEURISTIC_SCORES[key]) + ", ")
+#     #     print("\'" + key + " " + "\'" + " : " + str(HEURISTIC_SCORES[key]) + ", ")
+#     #     print("\'" + "x" + key + "\'" + " : " + str(HEURISTIC_SCORES[key]) + ", ")
+#     #     print("\'" + "o" + key + "\'" + " : " + str(HEURISTIC_SCORES[key]) + ", ")
+#     #     print("\'" + " " + key + "\'" + " : " + str(HEURISTIC_SCORES[key]) + ", ")
+#     # print("\'" + key + "\'" + " : " + str(HEURISTIC_SCORES[key]) + ", ")
 #     print(str(t) + " : " + str(HEURISTIC_SCORES[key]) + ",")
+
 
 # print(pow(R, 5) % Q)
 # print(pow(R, 4) % Q)
-# print(pow(R, 3) % Q)
 
 
 def get_move(board, size):
@@ -77,7 +245,10 @@ def get_move(board, size):
     alpha = float("-inf")
     beta = float("inf")
     lres = list()
-    for c in list(children):
+    testList = list(children)
+    # testList.remove((6, 9))
+    # testList.insert(0, (6, 9))
+    for c in testList:
         cur = node('o', c, board, children, 1, size, 0) # role o as min value
         t = cur.max_value(alpha, beta)
         lres.append((c, t))
@@ -132,10 +303,10 @@ def get_children(board, size):
     return res
 class node:
     def __init__(self, role, move, board, children, depth, size, lastCache):
-        start = time.time()
+        # start = time.time()
         global count
         count += 1
-        print(str(count) + " depth: " + str(depth))
+        # print(str(count) + " depth: " + str(depth))
         self.role = role
         self.move = move
         x = move[0]
@@ -156,18 +327,25 @@ class node:
                     if not((i, j) in self.children):
                         self.children.add((i, j))
                         self.to_remove.append((i, j))
-        diff =time.time() - start
-        print("init = " + '{0:.30f}'.format(diff))
-        if diff > 0.001:
-            print_caro_table(self.board)
-            input()
+        # diff = time.time() - start
+        # print("init = " + '{0:.30f}'.format(diff))
+        # global delay_time
+        # delay_time += diff
+
     def restore(self):
+        # start = time.time()
         self.board[self.move[0]][self.move[1]] = ' '
         for i in self.to_remove:
             self.children.remove(i)
         self.children.add(self.move)
+        # diff = time.time() - start
+        # print("restore = " + '{0:.30f}'.format(diff))
+        # global delay_time
+        # delay_time += diff
     def min_value(self, alpha, beta):
         if self.depth == max_depth:
+            global count_leaf
+            count_leaf += 1
             return self.cache
             # t = self.compute()
             # self.restore()
@@ -176,9 +354,7 @@ class node:
             self.restore()
             return self.cache
         value = float("inf")
-        # start = time.time()
         l = list(self.children)
-        # print("copy list = " + '{0:.30f}'.format(time.time() - start))
         for c in l:
             cur = node('o', c, self.board, self.children, self.depth + 1, len(self.board), self.cache)
             t = cur.max_value(alpha, beta)
@@ -186,11 +362,14 @@ class node:
             beta = min(beta, t)
             if (alpha >= beta):
                 break
+
         self.restore()
         return value
 
     def max_value(self, alpha, beta):
         if self.depth == max_depth:
+            global count_leaf
+            count_leaf += 1
             return self.cache
             # t = self.compute()
             # # self.restore()
@@ -216,23 +395,42 @@ class node:
         #     input()
         return value
     def computeCache(self, lastCache):
+        # start = time.time()
         x = self.move[0]
         y = self.move[1]
 
-        before = self.computeBoard()
+        before = self.computeBoardRabinKarp()
+        # before = self.computeBoardSubString()
 
         self.board[x][y] = self.role
 
-        after = self.computeBoard()
+        after = self.computeBoardRabinKarp()
+        # after = self.computeBoardSubString()
 
         self.board[x][y] = ' '
-
+        # diff = time.time() - start
+        # print("restore = " + '{0:.30f}'.format(diff))
+        # global delay_time
+        # delay_time += diff
         return lastCache + after - before
-    def computeBoard(self):
+    # def computeBoardSubString(self):
+    #     x = self.move[0]
+    #     y = self.move[1]
+    #     b = self.board
+    #     s = len(b)
+    #
+
+
+    def computeBoardRabinKarp(self):
         h = self.horizontal()
         v = self.vertical()
+        # start = time.time()
         rd = self.right_diagonal()
         ld = self.left_diagonal()
+        # diff = time.time() - start
+        # print("restore = " + '{0:.30f}'.format(diff))
+        # global delay_time
+        # delay_time += diff
         return h + v + rd + ld
 
     def horizontal(self):
@@ -249,16 +447,15 @@ class node:
             if j >= s: return res
             char = b[x][j]
             count+=1
-
-            if count < 5: t5 = (R * t5 + ord(char)) % Q
-            if count >= 5:
-                if count == 5:
-                    t5 = (R * t5 + ord(char)) % Q
-                else:
-                    t5 = (t5 + Q - R5 * ord(b[x][j - 5]) % Q) % Q
-                    t5 = (t5 * R + ord(char)) % Q
-                if (t5 in hashT): res += hashT[t5]
-
+            # if count < 5: t5 = (R * t5 + ord(char)) % Q
+            # if count >= 5:
+            #     if count == 5:
+            #         t5 = (R * t5 + ord(char)) % Q
+            #     else:
+            #         t5 = (t5 + Q - R5 * ord(b[x][j - 5]) % Q) % Q
+            #         t5 = (t5 * R + ord(char)) % Q
+            #     if (t5 in hashT): res += hashT[t5]
+            #
             if count < 6: t6 = (R * t6 + ord(char)) % Q
             if count >= 6:
                 if count == 6:
@@ -284,14 +481,14 @@ class node:
             char = b[i][y]
             count += 1
 
-            if count < 5: t5 = (R * t5 + ord(char)) % Q
-            if count >= 5:
-                if count == 5:
-                    t5 = (R * t5 + ord(char)) % Q
-                else:
-                    t5 = (t5 + Q - R5 * ord(b[i - 5][y]) % Q) % Q
-                    t5 = (t5 * R + ord(char)) % Q
-                if (t5 in hashT): res += hashT[t5]
+            # if count < 5: t5 = (R * t5 + ord(char)) % Q
+            # if count >= 5:
+            #     if count == 5:
+            #         t5 = (R * t5 + ord(char)) % Q
+            #     else:
+            #         t5 = (t5 + Q - R5 * ord(b[i - 5][y]) % Q) % Q
+            #         t5 = (t5 * R + ord(char)) % Q
+            #     if (t5 in hashT): res += hashT[t5]
 
             if count < 6: t6 = (R * t6 + ord(char)) % Q
             if count >= 6:
@@ -304,6 +501,7 @@ class node:
         return res
 
     def left_diagonal(self):
+
         x = self.move[0] - 5
         y = self.move[1] - 5
         b = self.board
@@ -318,14 +516,14 @@ class node:
             if i >= s or j >= s: return res
             char = b[i][j]
 
-            if count < 5: t5 = (R * t5 + ord(char)) % Q
-            if count >= 5:
-                if count == 5:
-                    t5 = (R * t5 + ord(char)) % Q
-                else:
-                    t5 = (t5 + Q - R5 * ord(b[i - 5][j - 5]) % Q) % Q
-                    t5 = (t5 * R + ord(char)) % Q
-                if (t5 in hashT): res += hashT[t5]
+            # if count < 5: t5 = (R * t5 + ord(char)) % Q
+            # if count >= 5:
+            #     if count == 5:
+            #         t5 = (R * t5 + ord(char)) % Q
+            #     else:
+            #         t5 = (t5 + Q - R5 * ord(b[i - 5][j - 5]) % Q) % Q
+            #         t5 = (t5 * R + ord(char)) % Q
+            #     if (t5 in hashT): res += hashT[t5]
 
             if count < 6: t6 = (R * t6 + ord(char)) % Q
             if count >= 6:
@@ -353,14 +551,14 @@ class node:
             if i >= s or j >= s: return res
             char = b[i][j]
 
-            if count < 5: t5 = (R * t5 + ord(char)) % Q
-            if count >= 5:
-                if count == 5:
-                    t5 = (R * t5 + ord(char)) % Q
-                else:
-                    t5 = (t5 + Q - R5 * ord(b[i + 5][j + 5]) % Q) % Q
-                    t5 = (t5 * R + ord(char)) % Q
-                if (t5 in hashT): res += hashT[t5]
+            # if count < 5: t5 = (R * t5 + ord(char)) % Q
+            # if count >= 5:
+            #     if count == 5:
+            #         t5 = (R * t5 + ord(char)) % Q
+            #     else:
+            #         t5 = (t5 + Q - R5 * ord(b[i + 5][j + 5]) % Q) % Q
+            #         t5 = (t5 * R + ord(char)) % Q
+            #     if (t5 in hashT): res += hashT[t5]
 
             if count < 6: t6 = (R * t6 + ord(char)) % Q
             if count >= 6:
@@ -371,6 +569,7 @@ class node:
                     t6 = (t6 * R + ord(char)) % Q
                 if (t6 in hashT): res += hashT[t6]
         return res
+
     # def compute(self):
     #     global count_leaf
     #     count_leaf += 1
@@ -638,22 +837,25 @@ tb = [
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 ]
-# start = time.time()
-# move = get_move(tb, 20)
-# print(move)
-# print(time.time() - start)
-# print(count)
-# print(count_leaf)
-# tb[move[0]][move[1]] = 'o'
+start = time.time()
+move = get_move(tb, 20)
+print(move)
+print(time.time() - start)
+print(count)
+print(count_leaf)
+tb[move[0]][move[1]] = 'o'
+print_caro_table(tb)
+print(delay_time)
 
 
-# print_caro_table(tb)
-t = 10000000
-c = 's'
-while True:
-    start = time.time()
-    c *= 10
-    diff = time.time() - start
-    print("init = " + '{0:.30f}'.format(diff))
-    if diff > 0.001:
-        input()
+# t = 10000000000000000
+# print(type(t))
+# c = 's'
+# while True:
+#     start = time.time()
+#     t *= 100
+#     diff = time.time() - start
+#     # print(str(t) + " = " + '{0:.30f}'.format(diff))
+#     if diff > 0.0000008:
+#         print(str(t) + " = " + '{0:.30f}'.format(diff))
+#         input()
